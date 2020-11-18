9912e5bd4a1a609d71e71054b8e0d7ef1b5a82e8
zakfilip <70323056+zakfilip@users.noreply.github.com>
2020-11-18 16:45:41 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;
+    else
+    {
+        Stocznia stocznia{};
+        int z = 0;
+        do
+        {
+            Statek* s1 = stocznia();
+            if (dynamic_cast<Zaglowiec*>(s1))++z;
+            towar = -(s1->transportuj());
+            delete s1;
+        } while (towar >= 0);
+        return z;
+    }
 }
